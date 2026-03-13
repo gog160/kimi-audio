dc2e8704970c356983b8e7a1ddd6dfaa72ccf9da
gog160 <gog.clans@gmail.com>
2026-03-13 17:33:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def get_audio():
     try:
         ydl_opts = {
             'quiet': False,
-            'format': 'bestaudio[ext=webm]/bestaudio/best',
+            'format': 'bestaudio/best',
             'noplaylist': True,
             }
         if COOKIES_FILE and os.path.exists(COOKIES_FILE):
